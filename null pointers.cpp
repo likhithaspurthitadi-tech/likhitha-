958e96804b9c6c1aae2b4eae0c0b7157abd1b7fc
likhithaspurthitadi-tech <likhithaspurthitadi@gmail.com>
2025-11-28 18:56:41 +0530

    Add files via upload

---
+++ null pointers.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int *ptr=NULL;
+	printf("\n the value of the ptr %x",ptr);
+	printf("\n the address of ptr %x",&ptr);
+	printf("\n the ptr printing value %d",*ptr);
+	return 0;
+}
